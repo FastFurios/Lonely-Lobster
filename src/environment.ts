ef8fbf747bc4f2e4f2d32058d88dc8eda8601e08
Gerold <g.lindorfer@dal.de>
2023-11-05 17:43:15 +0100

    environment file updated to 2.2.2

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     version: "2.2.2",
-    notes: "1. warning if  value degration function is not known, or selection sort order parameters"
+    notes: "1. warning if  value degration function is not known, 2. thows error if selection measure and sort order parameters are unknown"
   }
