e7adbd203b0312704bd53609d200e9bbf199637b
Gerold <g.lindorfer@dal.de>
2023-12-10 11:57:42 +0100

    set version parameter

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    version: "3.0.0 alpha",
-    notes: "complex adaptive system learning - first release"
+    version: "3.1.0",
+    notes: "complex adaptive system learning - learn which out of a set og fixed strategies works best for me as an individual worker"
   }
