dcf4a2cd552cd4d6ee5ba1859f2e7d2b921a34c5
Gerold <gerold@lindorfer.com>
2025-03-09 17:18:01 +0100

    hide away tenant references

--- src/environment.ts
+++ src/environment.ts
@@ -11,8 +11,7 @@ export const environment = {
     notes: "more production readiness",
       // msal config details
     msalConfig: {
-      tenant:"49bf30a4-54b2-47ae-b9b1-ffa71ed3d475",
-      applicationId: "5797aa9c-0703-46d9-9fba-934498b8e5d6"
+      tenant:"<use your own Azure tenant>",
+      applicationId: "<use your own Azure application ID for the backend>"
     }
-
   }
