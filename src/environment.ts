911883a6056b6d1258021feeab2944fcd9bde45b
Gerold <gerold@lindorfer.com>
2025-03-16 17:44:34 +0100

    .vscode/* excluded in .gitignore

--- src/environment.ts
+++ src/environment.ts
@@ -6,6 +6,7 @@
 // last code cleaning: 04.01.2025
 
 /** environment information of the Lonely Lobster backend */
+
 export const environment = {
     version: "7.0.1",
     notes: "more production readiness",
